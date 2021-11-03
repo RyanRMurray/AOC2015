556c254710f2da742253235b3a91d47a480608f9
Ryan Murray <ryanrmurray@hotmail.co.uk>
2021-11-03 13:04:21 +0000

    Day 01

--- src/solutions/day01.ts
+++ src/solutions/day01.ts
@@ -1,3 +1,27 @@
+function solve(input:string):[string,string]{
+    let floor : number = 0;
+    let entered = false;
+    let at = -1;
+
+    for(let i = 0; i < input.length; i++){
+        if(input[i] === "("){
+            floor++;
+        } else {
+            floor--;
+        }
+
+        if(floor == -1 && !entered){
+            at = i + 1;
+            entered = true;
+        }
+    }
+    return [floor.toString(),at.toString()];
+}
+
 export default function day01(input:string) : Answers{
-    return {part1:"test1",part2:"test2"};
+    let [p1,p2] = solve(input);
+    return {
+        part1:p1,
+        part2:p2
+    };
 }
